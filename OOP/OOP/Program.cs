c356fd3364788157007ffc7a35c33f98f5403cd4
Himanshu72 <36104578+Himanshu72@users.noreply.github.com>
2021-06-03 12:19:28 +0530

    jagged array added

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -29,7 +29,23 @@ namespace OOP
 
             int[,] ad2 = new int[3, 2];
             Console.WriteLine(ad2.Rank);
-            
+            // jagged array
+            int[][] jaggedArray1 = new int[2][];
+            jaggedArray1[0] = new int[3] { 1,2,3};
+            jaggedArray1[1] = new int[2] { 4,5 };
+
+            int[][] jaggedArray2 = new int[][]{
+                    new int [3]{ 1,2,3},
+                    new  int [4]{1,2,3,4}
+            };
+
+            foreach (int[] ele in jaggedArray2) { 
+                foreach(int e in ele){
+                    Console.Write(e+" ");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
