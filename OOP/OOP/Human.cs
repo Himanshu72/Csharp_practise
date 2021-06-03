49770f6027d9cbe6b4e23f7f2df94718d82d8a70
Himanshu72 <36104578+Himanshu72@users.noreply.github.com>
2021-06-03 10:45:27 +0530

    OOP

---
+++ OOP/OOP/Human.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Human
+    {
+        private string name{ get; set; }
+        private byte age;
+
+        byte Age { get => age; set {
+                if (value > 0)
+                    this.age = value;
+                else
+                    throw new Exception("Age must be greater then 0");
+            } }
+
+        
+        public Human(string name,byte age) {
+            this.name = name;
+            this.Age = age;
+        }
+        public Human() { }
+
+        public void introduce() {
+            Console.WriteLine("Hello, My name is {0} and  I am {1} year old",name,age);
+        }
+        ~Human() {
+            Console.WriteLine("Object Destroyed");
+           
+        }
+
+    }
+}
---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+             Human john = new Human("john",18);
+            john.introduce();
+
+            //Playing with Array and array list
+            int[] a = new int[3];
+            a[0]= 10;
+            a[1] = 20;
+            a[2] = 30;
+            int[] b = new int[] { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine(b.Length);
+            foreach (int x in a) {
+                Console.WriteLine(x);
+            }
+
+            int[,] d2 = new int[,]{
+                {1,2 ,3},
+                {4,5,6 },
+                { 7,8,9}
+            };
+
+            int[,] ad2 = new int[3, 2];
+            Console.WriteLine(ad2.Rank);
+            
+        }
+    }
+}
