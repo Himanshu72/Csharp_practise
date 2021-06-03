9cf3fd17be0b58b941b9731c0ce9486da999ff4d
Himanshu72 <36104578+Himanshu72@users.noreply.github.com>
2021-06-03 12:34:00 +0530

    Array list

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace OOP
 {
@@ -7,12 +8,12 @@ namespace OOP
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-             Human john = new Human("john",18);
+            Human john = new Human("john", 18);
             john.introduce();
 
-            //Playing with Array and array list
+            //Playing with Array 
             int[] a = new int[3];
-            a[0]= 10;
+            a[0] = 10;
             a[1] = 20;
             a[2] = 30;
             int[] b = new int[] { 1, 2, 3, 4, 5, 6 };
@@ -31,21 +32,37 @@ namespace OOP
             Console.WriteLine(ad2.Rank);
             // jagged array
             int[][] jaggedArray1 = new int[2][];
-            jaggedArray1[0] = new int[3] { 1,2,3};
-            jaggedArray1[1] = new int[2] { 4,5 };
+            jaggedArray1[0] = new int[3] { 1, 2, 3 };
+            jaggedArray1[1] = new int[2] { 4, 5 };
 
             int[][] jaggedArray2 = new int[][]{
                     new int [3]{ 1,2,3},
                     new  int [4]{1,2,3,4}
             };
 
-            foreach (int[] ele in jaggedArray2) { 
-                foreach(int e in ele){
-                    Console.Write(e+" ");
+            foreach (int[] ele in jaggedArray2) {
+                foreach (int e in ele) {
+                    Console.Write(e + " ");
                 }
                 Console.WriteLine();
             }
+            //ArrayList......
+            // Can store mix type of value
+            ArrayList arr = new ArrayList(5);
+            Console.WriteLine("Count:{0}", arr.Count);
+            arr.Add(1);
+            arr.Add(1.2);
+            arr.Add("Hello");
+            arr.Add('h');
+            arr.Add(555L);
+            arr.Remove(555L);
+            arr.RemoveAt(0);
+            foreach (object obj in arr) {
+                //print only string and char
+                if(obj is char || obj is string)
+                Console.WriteLine(obj);
 
+            }
         }
     }
 }
