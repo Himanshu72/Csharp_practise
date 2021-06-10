58d97b8203ee8e2b1b8f68e61151941cc14f12cb
Himanshu72 <36104578+Himanshu72@users.noreply.github.com>
2021-06-10 07:33:43 +0530

    threading git push

---
+++ threading/threading/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+namespace threading
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            /*  
+              Console.WriteLine("Hello World 1");
+              Thread.Sleep(1000);
+              Console.WriteLine("Hello World 2");
+              Thread.Sleep(1000);
+              Console.WriteLine("Hello World 3");
+            */
+
+            //new Thread(()=> {
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Hello World");
+            //}).Start();
+            //new Thread(() => {
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Hello World 1");
+            //}).Start();
+            //new Thread(() => {
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Hello World 2");
+            //}).Start();
+
+            //var taskCompletionSource = new TaskCompletionSource<bool>();
+            //new Thread(() => {
+            //    Thread.Sleep(1000);    
+            //    taskCompletionSource.TrySetResult(true); 
+            //}).Start();
+            //var result = taskCompletionSource.Task.Result;
+            //Console.WriteLine($"Result:{result}");
+
+            //ThreadPool.QueueUserWorkItem(o =>
+            //{
+            //    Console.WriteLine("THread Pool");
+            //});
+             Task.Run(() =>
+            {
+                Console.WriteLine("Task Running");
+            });
+        }
+    }
+}
