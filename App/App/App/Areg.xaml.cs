846141acd0a67b399aaa2c11301740b624fa60a8
Himanshu72 <36104578+Himanshu72@users.noreply.github.com>
2021-07-20 16:40:47 +0530

    Final UI

---
+++ App/App/App/Areg.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Areg : ContentPage
+    {
+        bool load = false;
+        public Areg()
+        {
+            InitializeComponent();
+        }
+
+        async private void BTN_Clicked(object sender, EventArgs e)
+        {
+            if (load == false)
+            {
+                //add loding list agent logic here
+                ActivityIndicator activityIndicator = new ActivityIndicator { IsRunning = true };
+                Parent.Children.Add(activityIndicator);           
+                await Task.Delay(2000);
+                activityIndicator.IsRunning = false;
+                Parent.Children.Remove(activityIndicator);
+                BTN.Text = "Submit";
+                AgentList.IsVisible = true;
+                load = true;
+            }
+            else { 
+            
+                // go to next page;
+            }
+
+        }
+    }
+}
---
+++ App/App/App/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using FFImageLoading.Forms;
+using FFImageLoading.Svg.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App
+{
+    public partial class MainPage : ContentPage
+    {
+        string cur = "Agent";
+        public MainPage()
+        {
+            InitializeComponent();
+            
+          
+        }
+
+         private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            
+            if (Ctrl.IsChecked )
+            {
+                cur = "Controller";
+                Img.Source= new EmbeddedResourceImageSource(new Uri("resource://App.Images.ctrl.svg"));
+                Txt.Text = "A Controller phone shows location of agent.";
+            }
+            else {
+                cur = "Agent";
+                Img.Source = new EmbeddedResourceImageSource(new Uri("resource://App.Images.agent.svg"));
+                
+                Txt.Text = "Agent phone send their location to Controller phone.";
+            }
+        }
+
+        private  async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (Ctrl.IsChecked)
+            {
+                await Navigation.PushAsync(new Reg());
+            }
+            else {
+                await Navigation.PushAsync(new Areg());
+            }
+           
+        }
+    }
+}
